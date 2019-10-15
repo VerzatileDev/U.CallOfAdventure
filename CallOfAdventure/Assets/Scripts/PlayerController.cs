dc3db4578e04ef730d2511197a6d1eb09bbd1851
Filip Nejedly <nejedlyf@coventry.ac.uk>
2019-10-15 15:42:58 +0100

    Revised the code again

--- CallOfAdventure/Assets/Scripts/PlayerController.cs
+++ CallOfAdventure/Assets/Scripts/PlayerController.cs
@@ -2,46 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-
-    public float moveSpeed;
-
-    private Animator anim;
-
+    // Private and Public variables
+    public float movementSpeed;
     private Rigidbody2D myRigidbody;
 
-    private bool playerMoving;
-
-    private Vector2 lastMove;
 
 
-	// Use this for initialization
-	void Start () {
-        anim = GetComponent<Animator>();
+    // Use this for initialization
+    void Start()
+    {
+
         myRigidbody = GetComponent<Rigidbody2D>();
-	}
+    }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
-        playerMoving = false;
 
+        // Movement of the player
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*moveSpeed, myRigidbody.velocity.y);
-            playerMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f); 
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, myRigidbody.velocity.y);
+
 
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical")*moveSpeed);
-            playerMoving = true;
-            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * movementSpeed);
 
 
         }
@@ -56,11 +48,7 @@ public class PlayerController : MonoBehaviour {
 
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
         }
-    
-    anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-    anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-    anim.SetBool("PlayrMoving",playerMoving);
-    anim.SetFloat("LastMoveX", lastMove.x);
-    anim.SetFloat("LastMoveY", lastMove.y);
+
+
     }
 }
