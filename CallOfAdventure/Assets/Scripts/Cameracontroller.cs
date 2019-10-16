29455ba1438df1a2a3f809cf52e51394d84be724
Fine <nejedlyf@coventry.ac.uk>
2019-10-16 17:08:43 +0100

    Check Out play button

--- CallOfAdventure/Assets/Scripts/Cameracontroller.cs
+++ CallOfAdventure/Assets/Scripts/Cameracontroller.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Cameracontroller : MonoBehaviour {
 
 
-
+    public float moveSpeed;
     public GameObject followTarget;
 
-    private Vector3 targetPos;
-    public float moveSpeed;
+    private Vector3 targetPosition;
+    
 
 
 
@@ -20,7 +20,7 @@ public class Cameracontroller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed - Time.deltaTime);
+        targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed - Time.deltaTime);
     }
 }
