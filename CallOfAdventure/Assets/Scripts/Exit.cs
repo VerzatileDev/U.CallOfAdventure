bd000dfcca7ab5c16bf46950085eaa6475b8079f
Fine <nejedlyf@coventry.ac.uk>
2019-10-17 22:34:32 +0100

    Functional Menus!

---
+++ CallOfAdventure/Assets/Scripts/Exit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+
+    public void closegame()
+    {
+        Application.Quit();
+
+    }
+
+}
