b18d2f9af7131ff6c5e3c06676a35875ceed2c9b
Fine <nejedlyf@coventry.ac.uk>
2019-10-27 18:57:38 +0000

    Sound Slider now working properly

---
+++ CallOfAdventure/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class AudioScript : MonoBehaviour {
+
+
+    public AudioMixer mixer;
+
+
+    public void SetVolume (float sliderValue)
+
+    {
+        mixer.SetFloat("MasterSound", Mathf.Log10(sliderValue) * 20);
+    }
+}
