cf68fb9ab807a10a36c6d30d7167e67dae569bab
themelik <themelik@coventry.ac.uk>
2019-11-10 14:35:15 +0000

    Minor Fixes

--- CallOfAdventure/Assets/Scripts/PauseMenu.cs
+++ CallOfAdventure/Assets/Scripts/PauseMenu.cs
@@ -3,52 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
-{
+public class PauseMenu : MonoBehaviour {
 
-        // Update is called once per frame
-        void Update()
+    public static bool GameisPaused = false;
+
+    public GameObject pauseMenuUI;
+    
+    // Update is called once per frame
+    void Update() { 
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Input.GetKeyDown("escape"))
+            if (GameisPaused)
             {
-                if (GameisPaused)
-                {
-                    Resume();
-                }
-                else
-                {
-                    Pause();
-                }
+                Resume();
+            }else
+            {
+                Pause();
             }
         }
-
-        public void Resume()
-        {
-            pauseMenuUI.SetActive(false);
-            Time.timeScale = 1f;
-            GameisPaused = false;
-        }
-
-        void Pause()
-        {
-            pauseMenuUI.SetActive(true);
-            Time.timeScale = 0f;
-            GameisPaused = true;
-        }
-
-        public void LoadInGameOptions()
-        {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("InGameOptions");
-        }
-
-        public void closegame()
-        {
-            Application.Quit();
-
-        public static bool GameisPaused = false;
-
-        public GameObject pauseMenuUI;
     }
 
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+
+    public void LoadInGameOptions()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("InGameOptions");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
 }
