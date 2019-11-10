393de2a90a1a15f28a1f298843485d0d991f2c4a
themelik <themelik@coventry.ac.uk>
2019-11-10 13:45:41 +0000

    In Game Menu Almost ready

---
+++ CallOfAdventure/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown("escape"))
+            {
+                if (GameisPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameisPaused = false;
+        }
+
+        void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameisPaused = true;
+        }
+
+        public void LoadInGameOptions()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("InGameOptions");
+        }
+
+        public void closegame()
+        {
+            Application.Quit();
+
+        public static bool GameisPaused = false;
+
+        public GameObject pauseMenuUI;
+    }
+
+}
