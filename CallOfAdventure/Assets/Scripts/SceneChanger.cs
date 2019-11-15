8443c7a7b31dc6e43179d142ad301f969e1c65b8
Brian Latt <lattb@coventry.ac.uk>
2019-11-15 10:58:29 +0000

    Fader

--- CallOfAdventure/Assets/Scripts/SceneChanger.cs
+++ CallOfAdventure/Assets/Scripts/SceneChanger.cs
@@ -1,13 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 
 public class SceneChanger : MonoBehaviour {
-    
-    public void changemenuscene(string scenename)
-    {
-        Application.LoadLevel(scenename);
-      
-    }
+
+	public Animator animator;
+
+	private int levelToLoad;
+
+	void update (){
+		//if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == FighScene
+			//{
+	//}
+
+			}
+
+
+	public void changemenuscene(string scenename)
+	{
+		Application.LoadLevel(scenename);
+
+	}
+
+
+	// Fades between Scene/Level changes.
+
+	// Animator For Fader Between levels.
+	//FaderOut Is not used, In this but can be used for the Future ones
+
+
+	//public void FaderOnLevels (int levelIndex)
+	//{
+		//levelToLoad = levelIndex;
+		//animator.SetTrigger ("FaderOut");
+
+	//}
+	public void FaderComplete ()
+	{
+		SceneManager.LoadScene (levelToLoad);
+	}
 }
