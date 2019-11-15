b518667143311e247216855cc65ab0208533713d
Filip Nejedly <nejedlyf@coventry.ac.uk>
2019-11-15 10:56:20 +0000

    useless script

--- CallOfAdventure/Assets/Scripts/PlayerValues.cs
+++ CallOfAdventure/Assets/Scripts/PlayerValues.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApplyButton : MonoBehaviour {
+public class PlayerValues : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ CallOfAdventure/Assets/Scripts/SavingFile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+public static class SaveFile
+{
+	public static void SavePlayer (GameObject player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.save";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+    public static PlayerData LoadPlayer ()
+    {
+        string path = Application.persistentDataPath + "/player.save";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formater = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formater.Deserialize(stream) as PlayerData;
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+
+        }
+    }
+
+
+
+
+
+
+
+}
