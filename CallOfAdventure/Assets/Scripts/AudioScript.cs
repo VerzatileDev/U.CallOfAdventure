4922bc485d93922d7ac62708801fb37c2ef38a0f
Brianlatt <47317248+Brianlatt@users.noreply.github.com>
2019-11-24 15:41:44 +0000

    audio in the game with problems

--- CallOfAdventure/Assets/Scripts/AudioScript.cs
+++ CallOfAdventure/Assets/Scripts/AudioScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 //This script ties the Volume to a Slider inside a scene
 
