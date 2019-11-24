606e176fa8f1e6c6ba3a61de5c5668f6311d04a3
themelik <themelik@coventry.ac.uk>
2019-11-24 16:43:15 +0000

    Audio Scripts Small changes

---
+++ CallOfAdventure/Assets/Scripts/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public void changemenuscene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+
+    }
+
+    public int health = 40;
+
+}
