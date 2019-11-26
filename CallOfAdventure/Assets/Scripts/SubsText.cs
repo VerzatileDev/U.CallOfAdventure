9ed10c8c62079373c0f628885439f773b7698aa9
Brian Latt <lattb@coventry.ac.uk>
2019-11-26 13:40:16 +0000

    Subtitles, dont mind the rest Somehow uses wrong unity version.

---
+++ CallOfAdventure/Assets/Scripts/SubsText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubsText : MonoBehaviour {
+
+	public GameObject textbox;
+
+
+	void Start () {
+		StartCoroutine (TheSequence ());
+	}
+	IEnumerator TheSequence(){
+		yield return new WaitForSeconds (1);
+		textbox.GetComponent<Text> ().text = "Welcome To Call Of Adventure";
+		
+		yield return new WaitForSeconds (2);
+			textbox.GetComponent<Text>().text = "";
+		yield return new WaitForSeconds (1);
+		textbox.GetComponent<Text> ().text = "Hope you Enjoy The Alpha stage of the Game";
+		yield return new WaitForSeconds (2);
+		textbox.GetComponent<Text>().text = "";
+	}
+
+}
